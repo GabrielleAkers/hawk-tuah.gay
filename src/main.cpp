89ad9936669240b3686bef960a8d4d55f684a7f9
Gabrielle Akers (She/Her) <gabby.augusta.akers@gmail.com>
2026-06-12 11:43:32 -0700

    getting fullscreen working was actually a pain in the ass LOL what the fuck

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,35 @@
 #include "raylib.h"
 
 #if defined(PLATFORM_WEB)
-    #include <emscripten/emscripten.h>
+#include <emscripten/emscripten.h>
 #endif
 
-constexpr auto SCREEN_WIDTH  = 800;
-constexpr auto SCREEN_HEIGHT = 450;
+int screenWidth = 1200;
+int screenHeight = 800;
+
+extern "C" {    
+    EMSCRIPTEN_KEEPALIVE
+    extern void setWindowSize(const int width, const int height) {
+        screenWidth = width;
+        screenHeight = height;
+    }
+}
 
 Texture2D texture;
 
 static void UpdateDrawFrame(void);
 
-int main()
-{
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Window title");
-
-    texture = LoadTexture(ASSETS_PATH"wow_logo_icon.png");
+int main() {
+    InitWindow(screenWidth, screenHeight, "Hawk Tuah! 🏳️‍⚧️");
+    
+    texture = LoadTexture(ASSETS_PATH "wow_logo_icon.png");
 
 #if defined(PLATFORM_WEB)
-    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
+    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
 #else
     SetTargetFPS(60);
 
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()) {
         UpdateDrawFrame();
     }
 #endif
@@ -39,13 +45,14 @@ static void UpdateDrawFrame(void) {
 
     ClearBackground(RAYWHITE);
 
-    const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
-    const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
+    const int texture_x = screenWidth / 2 - texture.width / 2;
+    const int texture_y = screenHeight / 2 - texture.height / 2;
     DrawTexture(texture, texture_x, texture_y, WHITE);
 
     const char* text = "OMG! IT WORKS!";
     const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
-    DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
+    DrawText(text, screenWidth / 2 - text_size.x / 2,
+             texture_y + texture.height + text_size.y + 10, 20, BLACK);
 
     EndDrawing();
 }

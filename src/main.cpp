89e004b53ce124b08b6c2c5b5472c5d6b64adb78
Gabrielle Akers (She/Her) <gabby.augusta.akers@gmail.com>
2026-06-14 06:07:36 -0700

    ball bounce

--- src/main.cpp
+++ src/main.cpp
@@ -58,6 +58,7 @@ static JPH::BodyID CreateFloorAndSphere(const float sphere_radius) {
         sphere_settings, JPH::EActivation::Activate);
 
     body_interface.SetLinearVelocity(sphere_id, JPH::Vec3(0.0f, -5.0f, 0.0f));
+    body_interface.SetRestitution(sphere_id, 0.8f);
 
     physics_manager->physics_system->OptimizeBroadPhase();
 
